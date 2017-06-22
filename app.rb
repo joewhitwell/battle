93ef232f38a8495dc46cbf55b1a1d2092a17fc67
Joe Whitwell <joewhitwell@hotmail.com>
2017-06-22 12:45:33 +0100

    First user story with tests

--- app.rb
+++ app.rb
@@ -1,7 +1,24 @@
-require 'sinatra'
+require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  # p 1
   get '/' do
-    'Testing infrastructure working!'
+    # p 2
+   erb :index
+  #  p 3
   end
+
+  post '/fight' do
+    # p 4
+    @P1 = params[:Player1]
+    # p 5
+    @P2 = params[:Player2]
+    # p 6
+    erb :fight
+    # p 7
+  end
+
+
+
+run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,23 @@
-describe "Testing Infrastructure", :type => :feature do
-  it "returns Testing infrastructure working!" do
-    visit ('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+describe "Name input" do
+  feature "form exists" do
+    scenario "contains a player one field signifying name input form" do
+      visit ('/')
+      expect(page).to have_content 'Go fight'
+      expect(page).to have_content 'Player 2'
+    end
   end
 end
+
+describe "Render name" do
+  feature "post names to /fight page" do
+    scenario "Enter name, and view on fight page" do
+      visit ('/')
+      fill_in "Player1", :with => "Joe"
+      click_on 'Go fight'
+      expect(page).to have_content 'Joe'
+    end
+  end
+end
+
+
+# fill_in 'Title', with: 'I love Rails!'
